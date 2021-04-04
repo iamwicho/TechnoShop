baa9d05ff38e8b7e21b40e27f3c5742156f90e03
iamwicho <70763653+iamwicho@users.noreply.github.com>
2021-04-04 16:25:23 -0500

    Formularios terminados | Referencias
    Diseño listo de formularios tablas
    
    CRUD V1

--- AdminLTE-3.1.0-rc/pages/tables/TablaCliente.php
+++ AdminLTE-3.1.0-rc/pages/tables/TablaCliente.php
@@ -45,7 +45,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4 ">
             <!-- Brand Logo -->
             <a href="../../admin.php" class="brand-link">
-                <img src="/AdminLTE-3.1.0-rc/assets/index/miniLogo.png" alt="TechnoShop Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
+                <img src="../AdminLTE-3.1.0-rc/assets/index/miniLogo.png" alt="TechnoShop Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
                 <span class="brand-text font-weight-light">TechnoShop</span>
             </a>
 
